5e639b76af6a99e95f0394f8e7ca58050611bde9
Chaitanya <chaitu.raju@gmail.com>
2025-04-07 23:31:15 +0530

    upgraded next

--- app/api/auth/[...nextAuth]/route.js
+++ app/api/auth/[...nextAuth]/route.js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log("GOOGLE_ID:", process.env.GOOGLE_ID);
-console.log("GOOGLE_SECRET:", process.env.GOOGLE_SECRET);
 
 const handler = NextAuth({
   providers: [
